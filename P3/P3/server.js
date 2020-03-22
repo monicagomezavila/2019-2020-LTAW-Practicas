61d40ace1e3d77515d1e752f2063cfa2f7c04bb8
monicagomezavila <monicagomezavila@github.com>
2020-03-22 09:01:13 +0100

    Página index dinámica, se cambia en el user, no se entrega directamente

--- P3/P3/server.js
+++ P3/P3/server.js
@@ -1,82 +1,107 @@
+//-- Puerto donde recibir las peticiones
+const PUERTO = 8080;
+
+//-- Modulos
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
-const PUERTO = 8080
-
-//-- Configurar y lanzar el servidor
-
-http.createServer((req, res) => {
-  let q = url.parse(req.url, true);
-  var cookie = req.headers.cookie;
-  //console.log("----------> Peticion recibida")
-  //console.log("Petición:" + q.pathname)
-  //console.log("Cookie: " + cookie)
-
-  //-- Variables
-  let filename = ""; //-- Recurso
-  let mime = ""; //-- Tipo de recurso
+const fs = require('fs')
 
 
-  //-- Se accede a la pagina
-  if (q.pathname == "/"){
-    mime = "text/html"
+//-- Funcion para atender a una peticion
+function peticion(req, res) {
 
-    if (!cookie) {
-      filename += "registro.html";
+  //-- Mostrar en la consola el recurso al que se accede
+  const q = url.parse(req.url, true);
+  console.log("Petición: " + q.pathname)
 
-    //-- Hay definida una Cookie.
-    } else {
-       filename += "index.html";
-     }
+  //-- Leer cookies
+  const cookie = req.headers.cookie;
+  console.log("Cookie: " + cookie)
 
-   }else if (q.pathname == ('/index.html')) {
-     //-- Si han pulsado el enlace del sistema de registro
-     if (!cookie) {
-      res.setHeader('Set-Cookie', 'user=MON')
+  var filename = ""
+  var mime = ""
+
+
+  switch (q.pathname) {
+
+    //-- Pagina principal
+    case "/":
+    //-- GENERO UN HTML SEGUN LA CONDICION DE LAS COOKIES
+      content = fs.readFileSync("index.html", "utf-8")
+      mime = "text/html"
+      //-- No hay ninguna cookie
+      if (!cookie) {
+        content += "<div id = 'registro'><a href=/login><h2> SISTEMA DE REGISTRO</h2></a></div></div></body></html>"
+
+      //-- Hay definida una Cookie
+      } else {
+        equal_num = cookie.lastIndexOf("=");
+        user_cookie = cookie.slice(equal_num+1)
+        content += "<div id = 'registrado'><p> Bienvenido a mi tienda " + user_cookie + "</hp></div></div></body></html>"
       }
 
+      res.writeHead(200, {'Content-Type': mime});
+      res.write(content);
+      res.end();
+
+      break;
+
+    case "/login":
       mime = "text/html"
-      q.pathname = "/index.html"
-      filename = "." + q.pathname
+      res.setHeader('Set-Cookie', 'user=MONICA')
+      content = "Registrado! Cookie enviada al navegador."
+      content += "<a href=/><h2> PAGINA PRINCIPAL</h2></a>"
+      res.writeHead(200, {'Content-Type': mime});
+      res.write(content);
+      res.end();
+      break;
 
-    }else{
-      //-- Si encuentra más de una "/" esta en otra carpeta
-      let cant = 0; //-- Variable que guarda la cantidad de "/" encontradas
 
+    default:
+
+      //-- Peticiones de estilo, imágenes, etc...
+      let cant = 0;
+      //-- Si hay mas de una /, significa que esta dentro de una carpeta
       for(var i = 0; i < q.pathname.length; i++) {
-    	 if (q.pathname[i] == "/")
+         if (q.pathname[i] == "/")
             cant = cant+1;
        }
 
-       //-- Dependiendo del valor de cant se decido el path que tiene el recurso
        if (cant>1){
           filename = "." + q.pathname
-      }else{
+       }else{
           filename = q.pathname.slice(1)
-      }
-      //-- Para saber el tipo mime del recurso, se mira la parte final del mismo
+       }
+
+      //-- Encontrar tipo mime
       num = q.pathname.lastIndexOf(".");
       mime = q.pathname.slice(num+1)
       mime = "text/" + mime
+
+      fs.readFile(filename, function(err, data) {
+
+        //-- Fichero no encontrado. Devolver mensaje de error
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
+        }
+
+        //-- Generar el mensaje de respuesta
+        res.writeHead(200, {'Content-Type': mime});
+        res.write(data);
+        res.end();
+      });
   }
 
+}
 
-  //-- Leer fichero
-  fs.readFile(filename, function(err, data) {
+//-- Inicializar el servidor
+//-- Cada vez que recibe una petición
+//-- invoca a la funcion peticion para atenderla
+const server = http.createServer(peticion)
 
-    //-- Fichero no encontrado. Devolver mensaje de error
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
+//-- Configurar el servidor para escuchar en el puerto
+server.listen(PUERTO);
 
-    //-- Generar el mensaje de respuesta
-    res.writeHead(200, {'Content-Type': mime});
-    res.write(data);
-    res.end();
-  });
-
-}).listen(PUERTO);
-
-console.log("Servidor corriendo...")
-console.log("Puerto: " + PUERTO)
+console.log("Servidor LISTO!")
+console.log("Escuchando en puerto: " + PUERTO)
